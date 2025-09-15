8fecbbf24dc2c5939c5cf3e706c1b87ddc8a3465
Sip4818 <sip4818@gmail.com>
2025-09-15 16:07:07 +0530

    data transformation done

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
 from network_security.components.data_ingestion import DataIngestion
+from network_security.components.data_validation import DataValidation
+from network_security.components.data_transformation import DataTransformation
 from network_security.entity.config_entity import DataIngestionConfig
+from network_security.entity.config_entity import DataValidationConfig
+from network_security.entity.config_entity import DataTransformationConfig
 from network_security.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -14,6 +18,21 @@ if __name__=="__main__":
         dataingestion=DataIngestion(dataingestionconfig)
         dataingestionartifact=dataingestion.initiate_data_ingestion()
         print(dataingestionartifact)
+        logging.info("Data Ingestion Completed")
+        datavalidationconfig=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,datavalidationconfig)
+        logging.info("Inititate data validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("Data validation complete")
+        print(data_validation_artifact)
+        datatransformationconfig=DataTransformationConfig(trainingpipelineconfig)
+        datatransformation=DataTransformation(data_validation_artifact,datatransformationconfig)
+        logging.info("Initiating data transformatin")
+        data_transformation_artifact=datatransformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+
+
+
         
     except Exception as e:
         raise NetworkSecurityException(e,sys)
---
+++ network_security/utils/main_utils/utils.py
@@ -0,0 +1,51 @@
+import yaml
+import sys,os
+from network_security.exception.exception import NetworkSecurityException
+from network_security.logging.logger import logging
+import numpy as np
+import pickle
+
+def read_yaml_file(file_path: str)->dict:
+    try:
+        path_dir=os.path.dirname(file_path)
+        os.makedirs(path_dir,exist_ok=True)
+        with open(file_path,'r') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+    
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info("Entered the save_object method of MainUtils class")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info("Exited the save_object method of MainUtils class")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
